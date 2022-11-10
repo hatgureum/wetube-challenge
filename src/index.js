61d87842a2f229487a0827e024097b3b1114042a
hatgureum <73741078+hatgureum@users.noreply.github.com>
2022-11-10 10:04:32 +0900

    initial

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import "./db";
+import express from "express";
+import path from "path";
+import bodyParser from "body-parser";
+import movieRouter from "./movieRouter";
+import { localsMiddleware } from "./middlewares";
+
+const app = express();
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(localsMiddleware);
+app.use("/", movieRouter);
+
+// Codesanbox does not need PORT :)
+const PORT = 4001;
+app.listen(PORT, () =>
+  console.log(`✅  Server Ready! http://localhost:${PORT}`)
+);
