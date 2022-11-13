fca00d5c06d7ccd91c2c74f552afe9d6e74c9d80
hatgureum <73741078+hatgureum@users.noreply.github.com>
2022-11-13 14:42:46 +0900

    Day 48 Complete

--- src/db.js
+++ src/db.js
@@ -1,22 +1,12 @@
 import mongoose from "mongoose";
-/*
-DONT TOUCH THIS FILE <3
-WE ARE ALL SHARING THE SAME DB
-PLEASE DONT TOUCH THIS FILE
-*/
-mongoose.connect(
-  "mongodb+srv://wetubeadmin:wetube4ever@cluster0.uufes.mongodb.net/wetube?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-    useFindAndModify: false
-  }
-);
+
+mongoose.connect("mongodb://127.0.0.1:27017/wetube", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: true,
+});
 
 const db = mongoose.connection;
 
-const handleOpen = () => console.log("✅  Connected to DB");
-const handleError = (error) =>
-  console.log(`❌ Error on DB Connection:${error}`);
-
-db.once("open", handleOpen);
-db.on("error", handleError);
+db.on("error", (error) => console.log("❌ DB Error:", error));
+db.once("open", () => console.log("✔ Connected to DB"));
--- src/movieRouter.js
+++ src/movieRouter.js
@@ -1,7 +1,23 @@
 import express from "express";
+import {
+  home,
+  getUpload,
+  postUpload,
+  search,
+  movieDetail,
+  getEdit,
+  postEdit,
+  movieDelete,
+} from "./movieController";
 
 const movieRouter = express.Router();
 
 // Add your magic here!
+movieRouter.get("/", home);
+movieRouter.route("/upload").get(getUpload).post(postUpload);
+movieRouter.get("/search", search);
+movieRouter.get("/movie/:id", movieDetail);
+movieRouter.route("/movie/:id/edit").get(getEdit).post(postEdit);
+movieRouter.get("/movie/:id/delete", movieDelete);
 
 export default movieRouter;
