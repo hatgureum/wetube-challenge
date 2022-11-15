41fba82c2ffdbfe09728481dfbcf0932048f5e9e
hatgureum <73741078+hatgureum@users.noreply.github.com>
2022-11-16 01:06:16 +0900

    Day 51 Start

--- src/db.js
+++ src/db.js
@@ -1,12 +1,22 @@
 import mongoose from "mongoose";
-
-mongoose.connect("mongodb://127.0.0.1:27017/wetube", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: true,
-});
+/*
+DONT TOUCH THIS FILE <3
+WE ARE ALL SHARING THE SAME DB
+PLEASE DONT TOUCH THIS FILE
+*/
+mongoose.connect(
+  "mongodb+srv://wetubeadmin:wetube4ever@cluster0.uufes.mongodb.net/wetube?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false
+  }
+);
 
 const db = mongoose.connection;
 
-db.on("error", (error) => console.log("❌ DB Error:", error));
-db.once("open", () => console.log("✔ Connected to DB"));
+const handleOpen = () => console.log("✅  Connected to DB");
+const handleError = (error) =>
+  console.log(`❌ Error on DB Connection:${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
--- src/models/User.js
+++ src/models/User.js
@@ -8,27 +8,9 @@ PLEASE FOLLOW THIS STEP
 WE NEED TO SHARE THE SAME DB SO NICO CAN CHECK OUT EVERYBODYS PROJECT.
 🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧
 */
-const YOUR_USERNAME = "hatgureum";
+const YOUR_USERNAME = "cryoon";
 
-const MovieSchema = mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  summary: {
-    type: String,
-    required: true,
-  },
-  year: {
-    type: Number,
-    required: true,
-  },
-  rating: {
-    type: Number,
-    required: true,
-  },
-  genres: [String],
-});
+const UserSchema = mongoose.Schema({});
 
 if (YOUR_USERNAME === null || typeof YOUR_USERNAME !== "string") {
   /*
@@ -45,6 +27,6 @@ if (YOUR_USERNAME.includes("@")) {
   throw Error("❌  Please remove the @ from your username  ❌");
 }
 
-const model = mongoose.model(`Movie_${YOUR_USERNAME}`, MovieSchema);
+const model = mongoose.model(`User_${YOUR_USERNAME}`, UserSchema);
 
 export default model;
