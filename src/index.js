9e5eef09d7c6501f458a7ac76d96490a7243d9ad
hatgureum <73741078+hatgureum@users.noreply.github.com>
2022-11-26 11:58:00 +0900

    save

---
+++ src/index.js
@@ -0,0 +1,48 @@
+const recordBtn = document.querySelector("#recordBtn");
+const audio = document.querySelector("#audio");
+
+let stream;
+let audioFile;
+let recorder;
+
+const handleDownload = () => {
+  const a = document.createElement("a");
+  a.href = audioFile;
+  a.download = "myAudio.weba";
+  document.body.appendChild(a);
+  a.click();
+};
+
+const handleStop = () => {
+  recordBtn.innerText = "Download Recording";
+  recordBtn.removeEventListener("click", handleStop);
+  recordBtn.addEventListener("click", handleDownload);
+  recorder.stop();
+};
+
+const handleStart = () => {
+  recordBtn.innerText = "Stop Recording";
+  recordBtn.removeEventListener("click", handleStart);
+  recordBtn.addEventListener("click", handleStop);
+
+  recorder = new MediaRecorder(stream);
+  recorder.ondataavailable = (event) => {
+    audioFile = URL.createObjectURL(event.data);
+    audio.srcObject = null;
+    audio.src = audioFile;
+    audio.play();
+  };
+  setTimeout(handleStop, 5000);
+};
+
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
+    audio: true,
+    video: false,
+  });
+  audio.srcObject = stream;
+};
+
+init();
+
+recordBtn.addEventListener("click", handleStart);
